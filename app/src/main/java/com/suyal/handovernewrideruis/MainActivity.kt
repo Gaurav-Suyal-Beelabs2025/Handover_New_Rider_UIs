52ab43c6ba1678ca1932b6c2864689e48ce49f0f
Saurabh Kumar <saurabhk@beelabs.technology>
2023-11-07 12:57:13 +0530

    earning ui

--- app/src/main/java/com/suyal/handovernewrideruis/MainActivity.kt
+++ app/src/main/java/com/suyal/handovernewrideruis/MainActivity.kt
@@ -2,10 +2,16 @@ package com.suyal.handovernewrideruis
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.suyal.handovernewrideruis.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
